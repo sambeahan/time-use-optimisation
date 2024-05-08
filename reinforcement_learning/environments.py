ed2167103323de2fc21e1effa6162c0ff5edd439
sambeahan <sam.beahan@gmail.com>
2024-05-08 23:27:31 +0930

    Added environment time constraints

--- reinforcement_learning/environments.py
+++ reinforcement_learning/environments.py
@@ -14,7 +14,10 @@ BMI_VAR_BONUS = 10
 
 
 def reward_add(current_obj, post_obj):
+    # Individual action reward:
     # return current_obj - post_obj
+
+    # Cumulative reward:
     return -1 * post_obj
 
 
@@ -23,8 +26,8 @@ class TimeUseEnv(gym.Env):
         # define bounds
         self.max_sleep = 12.0
         self.min_sleep = 4.0
-        self.max_sedentary = 20.0
-        self.min_sedentary = 2.0
+        self.max_sedentary = 18.0
+        self.min_sedentary = 1.0
         self.max_active = 10.0
         self.min_active = 0.5
 
@@ -37,9 +40,9 @@ class TimeUseEnv(gym.Env):
                 np.float32(self.min_active),
             ]
         )
-        obs_high = np.array([self.max_sleep, self.max_sedentary, self.max_active])
+        self.obs_high = np.array([self.max_sleep, self.max_sedentary, self.max_active])
 
-        self.observation_space = spaces.Box(low=self.obs_start, high=obs_high)
+        self.observation_space = spaces.Box(low=self.obs_start, high=self.obs_high)
 
         self.stress_last_action = 0
         self.hr_last_action = 0
@@ -55,57 +58,63 @@ class TimeUseEnv(gym.Env):
 
         return self.current_obs, {}
 
-    def step(self, action, agent):
+    def step(self, action, agent, is_training=False):
         next_obs = np.zeros(3)
 
         for i, time in enumerate(self.current_obs):
             next_obs[i] = np.float32(time)
 
-        next_obs[action] += np.float32(0.1)
+        # Only add action if below threshold
+        valid_action = False
+        if next_obs[action] + np.float32(0.1) <= self.obs_high[action] or is_training:
+            next_obs[action] += np.float32(0.1)
+            valid_action = True
 
         reward = 0
 
         current_obj = 0
         post_obj = 0
+
         if agent == "stress":
             current_obj = objective_functions.calc_stress(self.current_obs.tolist())
             post_obj = objective_functions.calc_stress(next_obs.tolist())
-            if action != self.stress_last_action:
+            if action != self.stress_last_action and valid_action:
                 reward += STRESS_VAR_BONUS
 
             self.stress_last_action = action
         elif agent == "hr":
             current_obj = objective_functions.calc_hr(self.current_obs.tolist())
             post_obj = objective_functions.calc_hr(next_obs.tolist())
-            if action != self.hr_last_action:
+            if action != self.hr_last_action and valid_action:
                 reward += HR_VAR_BONUS
 
             self.hr_last_action = action
         elif agent == "sbp":
             current_obj = objective_functions.calc_sbp(self.current_obs.tolist())
             post_obj = objective_functions.calc_sbp(next_obs.tolist())
-            if action != self.sbp_last_action:
+            if action != self.sbp_last_action and valid_action:
                 reward += SBP_VAR_BONUS
 
             self.sbp_last_action = action
         elif agent == "dbp":
             current_obj = objective_functions.calc_dbp(self.current_obs.tolist())
             post_obj = objective_functions.calc_dbp(next_obs.tolist())
-            if action != self.dbp_last_action:
+            if action != self.dbp_last_action and valid_action:
                 reward += DBP_VAR_BONUS
 
             self.dbp_last_action = action
         elif agent == "bmi":
             current_obj = objective_functions.calc_bmi(self.current_obs.tolist())
             post_obj = objective_functions.calc_bmi(next_obs.tolist())
-            if action != self.bmi_last_action:
+            if action != self.bmi_last_action and valid_action:
                 reward += BMI_VAR_BONUS
 
             self.bmi_last_action = action
 
         reward += reward_add(current_obj, post_obj)
 
-        self.time_left -= 0.1
+        if valid_action:
+            self.time_left -= 0.1
 
         done = False
         if self.time_left <= 0:
@@ -128,157 +137,27 @@ class TimeUseEnv(gym.Env):
 
 class StressEnv(TimeUseEnv):
     def step(self, action):
-        next_obs = np.zeros(3)
-
-        for i, time in enumerate(self.current_obs):
-            next_obs[i] = np.float32(time)
-
-        next_obs[action] += np.float32(0.1)
-
-        current_obj = objective_functions.calc_stress(self.current_obs.tolist())
-        post_obj = objective_functions.calc_stress(next_obs.tolist())
-
-        reward = current_obj - post_obj
-
-        self.time_left -= 0.1
-
-        if action != self.stress_last_action:
-            reward += STRESS_VAR_BONUS
-
-        self.stress_last_action = action
-
-        done = False
-        if self.time_left <= 0:
-            done = True
-
-        for i, time in enumerate(next_obs):
-            self.current_obs[i] = np.float32(time)
-
-        return self.current_obs, reward, done, False, {}
+        return super().step(action, "stress")
 
 
 class HREnv(TimeUseEnv):
     def step(self, action):
-        next_obs = np.zeros(3)
-
-        for i, time in enumerate(self.current_obs):
-            next_obs[i] = np.float32(time)
-
-        next_obs[action] += np.float32(0.1)
-
-        current_obj = objective_functions.calc_hr(self.current_obs.tolist())
-        post_obj = objective_functions.calc_hr(next_obs.tolist())
-
-        reward = current_obj - post_obj
-
-        if action != self.hr_last_action:
-            reward += HR_VAR_BONUS
-
-        self.hr_last_action = action
-
-        self.time_left -= 0.1
-
-        done = False
-        if self.time_left <= 0:
-            done = True
-
-        for i, time in enumerate(next_obs):
-            self.current_obs[i] = np.float32(time)
-
-        return self.current_obs, reward, done, False, {}
+        return super().step(action, "hr")
 
 
 class SBPEnv(TimeUseEnv):
     def step(self, action):
-        next_obs = np.zeros(3)
-
-        for i, time in enumerate(self.current_obs):
-            next_obs[i] = np.float32(time)
-
-        next_obs[action] += np.float32(0.1)
-
-        current_obj = objective_functions.calc_sbp(self.current_obs.tolist())
-        post_obj = objective_functions.calc_sbp(next_obs.tolist())
-
-        reward = current_obj - post_obj
-
-        if action != self.sbp_last_action:
-            reward += SBP_VAR_BONUS
-
-        self.sbp_last_action = action
-
-        self.time_left -= 0.1
-
-        done = False
-        if self.time_left <= 0:
-            done = True
-
-        for i, time in enumerate(next_obs):
-            self.current_obs[i] = np.float32(time)
-
-        return self.current_obs, reward, done, False, {}
+        return super().step(action, "sbp")
 
 
 class DBPEnv(TimeUseEnv):
     def step(self, action):
-        next_obs = np.zeros(3)
-
-        for i, time in enumerate(self.current_obs):
-            next_obs[i] = np.float32(time)
-
-        next_obs[action] += np.float32(0.1)
-
-        current_obj = objective_functions.calc_dbp(self.current_obs.tolist())
-        post_obj = objective_functions.calc_dbp(next_obs.tolist())
-
-        reward = current_obj - post_obj
-
-        if action != self.dbp_last_action:
-            reward += DBP_VAR_BONUS
-
-        self.dbp_last_action = action
-
-        self.time_left -= 0.1
-
-        done = False
-        if self.time_left <= 0:
-            done = True
-
-        for i, time in enumerate(next_obs):
-            self.current_obs[i] = np.float32(time)
-
-        return self.current_obs, reward, done, False, {}
+        return super().step(action, "dbp")
 
 
 class BMIEnv(TimeUseEnv):
     def step(self, action):
-        next_obs = np.zeros(3)
-
-        for i, time in enumerate(self.current_obs):
-            next_obs[i] = np.float32(time)
-
-        next_obs[action] += np.float32(0.1)
-
-        current_obj = objective_functions.calc_bmi(self.current_obs.tolist())
-        post_obj = objective_functions.calc_bmi(next_obs.tolist())
-
-        reward = current_obj - post_obj
-
-        if action != self.bmi_last_action:
-            reward += BMI_VAR_BONUS
-
-        self.bmi_last_action = action
-
-        self.time_left -= 0.1
-
-        done = False
-        if self.time_left <= 0:
-            done = True
-
-        for i, time in enumerate(next_obs):
-            self.current_obs[i] = np.float32(time)
-
-        return self.current_obs, reward, done, False, {}
+        return super().step(action, "bmi")
 
 
 if __name__ == "__main__":
--- reinforcement_learning/training.py
+++ reinforcement_learning/training.py
@@ -1,11 +1,14 @@
 from stable_baselines3 import A2C
 from environments import *
 
+STARTING_HOURS = 5.5
+TRAINING_EPISODES = 5
+
 agents = ["stress", "hr", "sbp", "dbp", "bmi"]
 envs = [StressEnv(), HREnv(), SBPEnv(), DBPEnv(), BMIEnv()]
 
 for i, agent in enumerate(agents):
     env = envs[i]
     model = A2C("MlpPolicy", env, verbose=1)
-    model.learn(total_timesteps=240 * 5)
+    model.learn(total_timesteps=(240 - STARTING_HOURS * 10) * TRAINING_EPISODES)
     model.save(f"reinforcement_learning/models/{agent}-agent")
