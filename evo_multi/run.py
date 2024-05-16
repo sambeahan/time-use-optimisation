92d5256d879c1e83ddc0687409d738e7dec60e8b
sambeahan <sam.beahan@gmail.com>
2024-05-16 10:33:30 +0930

    number based dominance selection

--- evo_multi/run.py
+++ evo_multi/run.py
@@ -1,7 +1,9 @@
+import random
+
 import objective_functions
 import ngsa_ii
 
-RUNS = 100
+RUNS = 1
 
 time_use_totals = [0, 0, 0]
 outcome_totals = {}
@@ -28,27 +30,64 @@ for i in range(RUNS):
         eta=1,
     )
 
-    solution = ngsa_time_use[0]
-    last_solution = ngsa_time_use[-1]
+    competitors = [i for _ in range(len(ngsa_time_use))]
+    random.shuffle(competitors)
+
+    # number based dominance
+    while len(competitors) > 1:
+        next_round = []
+        for i, solution1 in enumerate(competitors[::2]):
+            if i + 1 < len(competitors):
+                solution2 = competitors[i + 1]
+
+                obj_vals1 = objective_functions.calc_outcomes(
+                    ngsa_time_use[solution1][0],
+                    ngsa_time_use[solution1][1],
+                    ngsa_time_use[solution1][2],
+                )
+                obj_vals2 = objective_functions.calc_outcomes(
+                    ngsa_time_use[solution2][0],
+                    ngsa_time_use[solution2][1],
+                    ngsa_time_use[solution2][2],
+                )
+
+                wins = 0
+
+                for outcome, value in obj_vals1.items():
+                    if value > obj_vals2[outcome]:
+                        wins += 1
+
+                if wins >= len(obj_vals1) / 2:
+                    next_round.append(solution1)
+                else:
+                    next_round.append(solution2)
+
+            else:
+                next_round.append(solution1)
+
+        competitors = next_round.copy()
+
+    solution = ngsa_time_use[competitors[0]]
+    # last_solution = ngsa_time_use[-1]
 
     time_use = solution[:3]
-    last_time_use = last_solution[:3]
+    # last_time_use = last_solution[:3]
     outcomes = objective_functions.calc_outcomes(solution[0], solution[1], solution[2])
-    last_outcomes = objective_functions.calc_outcomes(
-        last_solution[0], last_solution[1], last_solution[2]
-    )
+    # last_outcomes = objective_functions.calc_outcomes(
+    # last_solution[0], last_solution[1], last_solution[2]
+    # )
 
     for outcome, value in outcomes.items():
         if outcome in outcome_totals:
             outcome_totals[outcome] += value
-            last_outcome_totals[outcome] += last_outcomes[outcome]
+            # last_outcome_totals[outcome] += last_outcomes[outcome]
         else:
             outcome_totals[outcome] = value
-            last_outcome_totals[outcome] = last_outcomes[outcome]
+            # last_outcome_totals[outcome] = last_outcomes[outcome]
 
     for j, time in enumerate(time_use):
         time_use_totals[j] += time
-        last_time_use_totals[j] += last_time_use[j]
+        # last_time_use_totals[j] += last_time_use[j]
 
 print("\n----- AVERAGES -----")
 
@@ -59,9 +98,9 @@ for outcome in outcome_totals:
     print(outcome, outcome_totals[outcome] / RUNS)
 
 
-print("\nLasts:")
-for time in last_time_use_totals:
-    print(time / RUNS)
+# print("\nLasts:")
+# for time in last_time_use_totals:
+# print(time / RUNS)
 
-for outcome in last_outcome_totals:
-    print(outcome, last_outcome_totals[outcome] / RUNS)
+# for outcome in last_outcome_totals:
+# print(outcome, last_outcome_totals[outcome] / RUNS)
