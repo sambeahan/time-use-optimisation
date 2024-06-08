4c6231c431a1ea336072c00aa90dcc29e3274bce
sambeahan <sam.beahan@gmail.com>
2024-06-08 12:02:15 +0930

    dqn agent choices

--- graph_generation/agent_choices.py
+++ graph_generation/agent_choices.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 PARENT_DIR = Path(__file__).resolve().parent.parent
-GRAPH_PATH = Path(PARENT_DIR, "graph_generation", "graphs", "ppo_choice.pdf")
+GRAPH_PATH = Path(PARENT_DIR, "graph_generation", "graphs", "dqn_choice.pdf")
 
 BAR_WIDTH = 0.5
 
@@ -14,11 +14,11 @@ ax.grid(axis="y", zorder=0)
 # create data
 x = ["Sleep", "Sedentary", "Active"]
 y1 = np.array([4, 1, 0.5])
-y2 = np.array([2.68, 1.02, 0])
-y3 = np.array([0.0, 3.70, 0])
-y4 = np.array([0, 3.15, 0.55])
-y5 = np.array([0, 1.91, 1.78])
-y6 = np.array([2.57, 0.39, 0.75])
+y2 = np.array([2.94, 0.47, 0.29])
+y3 = np.array([0.07, 3.57, 0.06])
+y4 = np.array([0.06, 1.85, 1.78])
+y5 = np.array([0.06, 1.38, 2.25])
+y6 = np.array([2.30, 0.55, 0.85])
 
 # plot bars in stack manner
 ax.bar(x, y1, width=BAR_WIDTH, color="#a6a6a6", label="Lower bound", zorder=3)
@@ -45,7 +45,7 @@ ax.bar(x, y6, width=BAR_WIDTH, bottom=y1 + y2 + y3 + y4 + y5, label="BMI", zorde
 plt.legend()
 plt.xlabel("Activity")
 plt.ylabel("Time (hours)")
-plt.title("Time use chosen by each PPO RL agent")
+plt.title("Time use chosen by each DQN RL agent")
 # plt.show()
 plt.savefig(GRAPH_PATH)
 
